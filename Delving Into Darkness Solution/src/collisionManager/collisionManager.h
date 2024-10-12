0d95a92850d03d76c8efe5c01aefc5073d0e5e13
Lappland <B4d4ssG04t@gmail.com>
2024-10-12 00:07:20 -0300

    Addition of Button and Collition
    added button structure to make it simpler and clean
    added a collision check for rectangle x reactangle
    moves some res into UI folder and deleted some ui elements that i had no use for

---
+++ Delving Into Darkness Solution/src/collisionManager/collisionManager.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace collisions
+{
+
+	bool rectangleXrectangle(float r1x, float r1y, float r1w, float r1h, float r2x, float r2y, float r2w, float r2h);
+
+}
--- Delving Into Darkness Solution/src/menu/menu.cpp
+++ Delving Into Darkness Solution/src/menu/menu.cpp
@@ -4,12 +4,15 @@
 #include "raylib.h"
 
 #include "Delving_Into_Darkness.h"
+#include "collisionManager/collisionManager.h"
+#include "button/button.h"
 
 namespace Menu
 {
+	button::createButton playBttn;
+
 	const int candleAmountFrames = 4;
 	const int bookAmountFrames = 19;
-	const int buttonframes = 2;
 	const int pointerOffSet = 10;
 	const int bookOpenFrames = 12;
 	const int bookSwapPageFrames = 7;
@@ -24,7 +27,6 @@ namespace Menu
 	Texture2D bookFrames[bookAmountFrames];
 	Texture2D candleFrames[candleAmountFrames];
 	Texture2D itchLogo;
-	Texture2D buttonTest[buttonframes];
 	Texture2D pointerTex;
 
 	Vector2 pointerPosition = {0.0f, 0.0f};
@@ -44,59 +46,64 @@ namespace Menu
 	bool bookOpenAnimationOn = true;
 	bool bookSwapRight = false;
 	bool bookSwapLeft = false;
-	bool buttonHover = false;
 
 	void initMenu()
 	{
-			image = LoadImage("res/BookDesk/grayDesk.png");     // Loaded in CPU memory (RAM)
-			ImageResize(&image, static_cast<int>(gameManager::Screen.size.x), static_cast<int>(gameManager::Screen.size.y)); // resize image before aplying to texture
-			menuBackground = LoadTextureFromImage(image);          // Image converted to texture, GPU memory (VRAM)
-			UnloadImage(image);   // Once image has been converted to texture and uploaded to VRAM, it can be unloaded from RAM
+		playBttn.amountOfFrames = 2; //   PLAY BTTN INITIALIZATION 
+		playBttn.buttonText[playBttn.amountOfFrames];
+		playBttn.buttonFrame = 0;
+		playBttn.position = { 340, 260 };
+		playBttn.buttonText = new Texture2D[playBttn.amountOfFrames]; 
+		playBttn.buttonText[0] = LoadTexture("res/ui/button/button01.png");
+		playBttn.buttonText[1] = LoadTexture("res/ui/button/button02.png");
+		button::assignWidthAndHeight(playBttn); //   PLAY BTTN INITIALIZATION END
 
-			//Book opens
-			bookFrames[0] = LoadTexture("res/OpenBook/1.png");
-			bookFrames[1] = LoadTexture("res/OpenBook/2.png");
-			bookFrames[2] = LoadTexture("res/OpenBook/3.png");
-			bookFrames[3] = LoadTexture("res/OpenBook/4.png");
-			bookFrames[4] = LoadTexture("res/OpenBook/5.png");
-			//Markers open from here on
-			bookFrames[5] = LoadTexture("res/OpenBook/6.png");
-			bookFrames[6] = LoadTexture("res/OpenBook/7.png");
-			bookFrames[7] = LoadTexture("res/OpenBook/8.png");
-			bookFrames[8] = LoadTexture("res/OpenBook/9.png");
-			bookFrames[9] = LoadTexture("res/OpenBook/10.png");
-			bookFrames[10] = LoadTexture("res/OpenBook/11.png");
-			bookFrames[11] = LoadTexture("res/OpenBook/12.png");  
-			//Page swap
-			bookFrames[12] = LoadTexture("res/OpenBook/13.png");
-			bookFrames[13] = LoadTexture("res/OpenBook/14.png");
-			bookFrames[14] = LoadTexture("res/OpenBook/15.png");
-			bookFrames[15] = LoadTexture("res/OpenBook/16.png");
-			bookFrames[16] = LoadTexture("res/OpenBook/17.png");
-			bookFrames[17] = LoadTexture("res/OpenBook/18.png");
-			bookFrames[18] = LoadTexture("res/OpenBook/19.png");
-			//ItchIO Logo 
-			itchLogo = LoadTexture("res/miscellaneous/itch_Io_logo.png");
-			//Candle Anim
-			candleFrames[0] = LoadTexture("res/candle/can5.png");
-			candleFrames[1] = LoadTexture("res/candle/can6.png");
-			candleFrames[2] = LoadTexture("res/candle/can7.png");
-			candleFrames[3] = LoadTexture("res/candle/can8.png");
-			//Button
-			buttonTest[0] = LoadTexture("res/button/button01.png");
-			buttonTest[1] = LoadTexture("res/button/button02.png");
-			//MousePointer
-			pointerTex = LoadTexture("res/miscellaneous/feather.png");
+		image = LoadImage("res/BookDesk/grayDesk.png");     // Loaded in CPU memory (RAM)
+		ImageResize(&image, static_cast<int>(gameManager::Screen.size.x), static_cast<int>(gameManager::Screen.size.y)); // resize image before aplying to texture
+		menuBackground = LoadTextureFromImage(image);          // Image converted to texture, GPU memory (VRAM)
+		UnloadImage(image);   // Once image has been converted to texture and uploaded to VRAM, it can be unloaded from RAM
 
-			//Audio
-			menuMusic = LoadMusicStream("res/sounds/TheVeilofNight.mp3");
-			bookOpen = LoadSound("res/sounds/openBook.wav");
-			flipPage = LoadSound("res/sounds/pageFlip.wav");
-			//Init AudioFiles
-			PlayMusicStream(menuMusic);
-			SetMusicVolume(menuMusic, 0.5f);
-			SetSoundVolume(bookOpen, 0.1f);
-			SetSoundVolume(flipPage, 0.3f);
+		//Book opens
+		bookFrames[0] = LoadTexture("res/OpenBook/1.png");
+		bookFrames[1] = LoadTexture("res/OpenBook/2.png");
+		bookFrames[2] = LoadTexture("res/OpenBook/3.png");
+		bookFrames[3] = LoadTexture("res/OpenBook/4.png");
+		bookFrames[4] = LoadTexture("res/OpenBook/5.png");
+		//Markers open from here on
+		bookFrames[5] = LoadTexture("res/OpenBook/6.png");
+		bookFrames[6] = LoadTexture("res/OpenBook/7.png");
+		bookFrames[7] = LoadTexture("res/OpenBook/8.png");
+		bookFrames[8] = LoadTexture("res/OpenBook/9.png");
+		bookFrames[9] = LoadTexture("res/OpenBook/10.png");
+		bookFrames[10] = LoadTexture("res/OpenBook/11.png");
+		bookFrames[11] = LoadTexture("res/OpenBook/12.png");
+		//Page swap
+		bookFrames[12] = LoadTexture("res/OpenBook/13.png");
+		bookFrames[13] = LoadTexture("res/OpenBook/14.png");
+		bookFrames[14] = LoadTexture("res/OpenBook/15.png");
+		bookFrames[15] = LoadTexture("res/OpenBook/16.png");
+		bookFrames[16] = LoadTexture("res/OpenBook/17.png");
+		bookFrames[17] = LoadTexture("res/OpenBook/18.png");
+		bookFrames[18] = LoadTexture("res/OpenBook/19.png");
+		//ItchIO Logo 
+		itchLogo = LoadTexture("res/miscellaneous/itch_Io_logo.png");
+		//Candle Anim
+		candleFrames[0] = LoadTexture("res/candle/can5.png");
+		candleFrames[1] = LoadTexture("res/candle/can6.png");
+		candleFrames[2] = LoadTexture("res/candle/can7.png");
+		candleFrames[3] = LoadTexture("res/candle/can8.png");
+		//MousePointer
+		pointerTex = LoadTexture("res/miscellaneous/feather.png");
+
+		//Audio
+		menuMusic = LoadMusicStream("res/sounds/TheVeilofNight.mp3");
+		bookOpen = LoadSound("res/sounds/openBook.wav");
+		flipPage = LoadSound("res/sounds/pageFlip.wav");
+		//Init AudioFiles
+		PlayMusicStream(menuMusic);
+		SetMusicVolume(menuMusic, 0.5f);
+		SetSoundVolume(bookOpen, 0.1f);
+		SetSoundVolume(flipPage, 0.3f);
 
 	} //END INIT =============================================================================================
 
@@ -120,6 +127,14 @@ namespace Menu
 		openBookAnim();
 
 		swapMenuPage();
+
+		if (collisions::rectangleXrectangle(playBttn.position.x, playBttn.position.y, playBttn.width, playBttn.height, pointerPosition.x, pointerPosition.y, static_cast<float>(pointerTex.width), static_cast<float>(pointerTex.height))){
+			playBttn.buttonFrame = 1;
+		}
+		else {
+			playBttn.buttonFrame = 0;
+		}
+
 
 		if (candleElapsedTime >= candleFrameTime) {
 			candleCurrentFrame = (candleCurrentFrame + 1) % candleAmountFrames; // Cycle to the next frame
@@ -149,7 +164,7 @@ namespace Menu
 
 		if (gameManager::CurrentScreen == gameManager::menu && currentFrame > 8 && currentFrame < 13) // MENU STATE AND NO ANIMATION 
 		{
-			DrawTexture(buttonTest[0], 340, 260, WHITE);
+			DrawTexture(playBttn.buttonText[playBttn.buttonFrame], static_cast<int>(playBttn.position.x), static_cast<int>(playBttn.position.y), WHITE);
 		}
 
 
@@ -158,7 +173,10 @@ namespace Menu
 		DrawTexture(pointerTex, static_cast<int>(pointerPosition.x) - pointerOffSet, static_cast<int>(pointerPosition.y) - pointerOffSet, WHITE);
 	} //END DRAW =============================================================================================
 
-	void unloadMenu()
+
+
+
+	void unloadMenu() // UNLOAD ================================================================================
 	{
 		system("cls");
 		std::cout << "MENU UNLOAD START --------------------------------------------------" << "\n";
@@ -172,8 +190,8 @@ namespace Menu
 		UnloadTexture(itchLogo);
 		UnloadTexture(pointerTex);
 		
-		for (int i = 0; i < buttonframes; i++) {
-			UnloadTexture(buttonTest[i]);
+		for (int i = 0; i < playBttn.amountOfFrames; i++) {
+			UnloadTexture(playBttn.buttonText[i]);
 		}
 
 		for (int i = 0; i < bookAmountFrames; i++) {
@@ -184,9 +202,7 @@ namespace Menu
 			UnloadTexture(candleFrames[i]);
 		}
 
-		for (int i = 0; i < buttonframes; i++) {
-			UnloadTexture(buttonTest[i]);
-		}
+		delete[] playBttn.buttonText;
 
 		std::cout << "MENU UNLOADED --------------------------------------------------" << "\n";
 
