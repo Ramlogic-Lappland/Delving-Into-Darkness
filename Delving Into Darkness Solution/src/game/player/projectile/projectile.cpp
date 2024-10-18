a0a43fba9b969e47e7d60ad7d182520fc01b593c
Lappland <b4d4ssg04t@gmail.com>
2024-10-18 12:59:16 -0300

    Edition to projectile system
    still needs work is nor working as intended

--- Delving Into Darkness Solution/src/game/player/projectile/projectile.cpp
+++ Delving Into Darkness Solution/src/game/player/projectile/projectile.cpp
@@ -7,12 +7,12 @@
 
 namespace Projectile
 {
-	void spawnProjectile(createProjectile& projectile, Player::CreatePlayer& player)
+	void spawnProjectile(createProjectile& projectile, Player::CreatePlayer& player, Vector2 dirVector)
 	{
-		projectile.position = {static_cast<float>(player.playerRect.x + (sin(player.rotation * DEG2RAD) * player.playerRect.height)), static_cast<float>(player.playerRect.y - (cos(player.rotation * DEG2RAD) *  player.playerRect.height))};
+		projectile.position = {static_cast<float>(player.playerRect.x + (sin(player.rotation * DEG2RAD) * player.playerRect.height)), static_cast<float>(player.playerRect.y - (cos(player.rotation * DEG2RAD) *  player.playerRect.height)) };
 		projectile.state = true;
-		projectile.speed.x = static_cast<float>(1.5f * sin(player.rotation * DEG2RAD) * player.speed.x);
-		projectile.speed.y = static_cast<float>(1.5f * cos(player.rotation * DEG2RAD) * player.speed.y);
+		projectile.speed.x = (dirVector.x * player.speed.x * GetFrameTime()) * -1;
+		projectile.speed.y = (dirVector.y * player.speed.y * GetFrameTime()) * -1;
 		projectile.rotation = player.rotation;
 	}
 }
